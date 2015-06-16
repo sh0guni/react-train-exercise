6e540d37fbaac16a67da495869553bb716fdc6ee
Jesse Korpela <jesse.korpela@solita.fi>
2015-06-16 10:13:48 +0300

    React-training step 1.5

--- src/app.js
+++ src/app.js
@@ -1,3 +1,7 @@
 var React = require('react');
 var TrainSearch = require('./components.jsx');
 
+React.render(
+    <TrainSearch />,
+    document.getElementById('app')
+);
--- src/components.jsx
+++ src/components.jsx
@@ -6,3 +6,28 @@ var trainStore = require('./store');
 var emptyTrain = require('./emptyTrain.json');
 var exampleTrain = require('./exampleTrain.json');
 
+var ScheduleTable = React.createClass({
+    getInitialState: function () {
+        return { train: exampleTrain }
+    },
+    render: function() {
+        return (
+            <div className="ScheduleTable">
+                Train number: {this.state.train.trainNumber}
+                <br/>Departure date: {this.state.train.departureDate}
+            </div>
+        )
+    }
+});
+
+var TrainSearch = React.createClass({
+    render: function () {
+        return (
+            <div className="TrainSearchApp">
+                <ScheduleTable />
+            </div>
+        )
+    }
+});
+
+module.exports = TrainSearch;
