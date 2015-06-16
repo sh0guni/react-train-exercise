6d0c31e7877770afb80cc54a75908ed417045511
Jesse Korpela <jesse.korpela@solita.fi>
2015-06-16 09:57:26 +0300

    React-training step 1

--- src/actions.js
+++ src/actions.js
@@ -10,15 +10,3 @@ function fetchTrain(train, callback) {
         callback(newTrain);
     });
 }
-
-var Actions = Reflux.createActions({
-    "trainSearch": {asyncResult: true}
-});
-
-Actions.trainSearch.listen(function (train) {
-    fetchTrain(train, function(newTrain) {
-        Actions.trainSearch.completed(newTrain);
-    });
-});
-
-module.exports = Actions;
--- src/app.js
+++ src/app.js
@@ -1,7 +1,3 @@
 var React = require('react');
 var TrainSearch = require('./components.jsx');
 
-React.render(
-    <TrainSearch />,
-    document.getElementById('app')
-);
