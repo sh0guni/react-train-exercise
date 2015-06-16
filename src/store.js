2e406efd4fb9424626ddc060091f465fcc6ca5fa
Jesse Korpela <jesse.korpela@solita.fi>
2015-06-16 10:50:33 +0300

    React-training step 1

--- src/store.js
+++ src/store.js
@@ -1,11 +1,2 @@
 var Reflux = require('reflux');
 var Actions = require('./actions');
-
-var trainStore = Reflux.createStore({
-    listenables: Actions,
-    onTrainSearchCompleted: function(train) {
-        this.trigger(train);
-    }
-});
-
-module.exports = trainStore;
