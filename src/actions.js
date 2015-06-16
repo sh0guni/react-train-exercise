f8b9b3ba445ed7705890ec5a5867a03d01286eae
Jesse Korpela <jesse.korpela@solita.fi>
2015-06-16 08:52:54 +0300

    Moved train fetching to separate function

--- src/actions.js
+++ src/actions.js
@@ -1,15 +1,23 @@
 var Reflux = require('reflux');
 var request = require('superagent');
 
+var emptyTrain = { trainNumber: '', departureDate: '', timeTableRows: [{stationShortCode: '', type: '', scheduledTime: null}]};
+
+function fetchTrain(train, callback) {
+    var url = 'http://rata.digitraffic.fi/api/v1/schedules/' + train.trainNumber + '?departure_date=' + train.departureDate;
+    request.get(url, function(err, res) {
+        if (err) throw err;
+        var newTrain = res.body.code ? emptyTrain : res.body;
+        callback(newTrain);
+    });
+}
+
 var Actions = Reflux.createActions({
     "trainSearch": {asyncResult: true}
 });
 
 Actions.trainSearch.listen(function (train) {
-    var url = 'http://rata.digitraffic.fi/api/v1/schedules/' + train.trainNumber + '?departure_date=' + train.departureDate;
-    request.get(url, function(err, res) {
-        if (err) throw err;
-        var newTrain = res.body.code ? emptyTrain : res.body;
+    fetchTrain(train, function(newTrain) {
         Actions.trainSearch.completed(newTrain);
     });
 });
