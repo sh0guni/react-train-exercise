79ab79cd471287684503b09e8047350690afecad
Jesse Korpela <jesse.korpela@solita.fi>
2015-10-07 18:18:40 +0300

    Reverted stores after wrong merge

--- src/store.js
+++ src/store.js
@@ -1,2 +1,11 @@
 var Reflux = require('reflux');
 var Actions = require('./actions');
+
+var trainStore = Reflux.createStore({
+    listenables: Actions,
+    onTrainSearchCompleted: function(train) {
+        this.trigger(train);
+    }
+});
+
+module.exports = trainStore;
